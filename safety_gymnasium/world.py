dc9c50bb828f01e2a62bf8df8e69146f18d65671
zmsn-2077 <jiamg.ji@gmail.com>
2023-06-27 01:06:29 +0800

    docs: update readme

--- safety_gymnasium/world.py
+++ safety_gymnasium/world.py
@@ -18,6 +18,7 @@ import os
 from collections import OrderedDict
 from copy import deepcopy
 from dataclasses import dataclass
+from typing import Any, ClassVar
 
 import mujoco
 import numpy as np
@@ -54,7 +55,7 @@ class World:  # pylint: disable=too-many-instance-attributes
 
     # Default configuration (this should not be nested since it gets copied)
     # *NOTE:* Changes to this configuration should also be reflected in `Builder` configuration
-    DEFAULT = {
+    DEFAULT: ClassVar[dict[str, Any]] = {
         'agent_base': 'assets/xmls/car.xml',  # Which agent XML to use as the base
         'agent_xy': np.zeros(2),  # agent XY location
         'agent_rot': 0,  # agent rotation about Z axis
